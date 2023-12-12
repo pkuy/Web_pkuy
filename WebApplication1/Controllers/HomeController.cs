07617cdb5fc48f18089524e91fd1d12de6726163
pkuy <phankhacuy@gmail.com>
2023-12-12 22:38:36 +0700

    Add project files.

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			ViewBag.text = "Welcome to my website !!!";
+			TempData["Num"] = 2101;
+
+			var data = new ErrorViewModel();
+			data.RequestId = "Test model !!!";
+            return View(data);
+		}
+
+		public IActionResult Privacy()
+		{
+            return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
